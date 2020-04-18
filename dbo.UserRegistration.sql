a888d8ab18a9f8eae27b05605223c330acb5c435
Salinda <salinda.jayasundara3010@gmail,com>
2020-04-18 22:18:44 +0530

    final commit of this code

---
+++ dbo.UserRegistration.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[UserID] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NULL, 
+    [LastName] VARCHAR(50) NULL, 
+    [Contact] VARCHAR(50) NULL, 
+    [Gender] VARCHAR(50) NULL, 
+    [Address] VARCHAR(50) NULL, 
+    [Username] VARCHAR(50) NULL, 
+    [Password] VARCHAR(50) NULL
+)
